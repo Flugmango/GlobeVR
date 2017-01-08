53b6b3e156b76c3dab6b703a93139a55926429ac
Nico Steffens <n.steffens@uni-muenster.de>
2017-01-08 16:27:01 +0100

    Introducing Infoscreens.
    - Added Basic Infoscreen implementation to visualize data
    - Screens are facing the main camera aka viewer
    - Needs to be tested in VR (..happening soon)

--- Assets/TestCameraScript.cs
+++ Assets/TestCameraScript.cs
@@ -1,11 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 using VRTK;
+using UnityEngine.UI;
+
 
 public class TestCameraScript : MonoBehaviour {
 
     public Camera camera;
     private static float sphereRadius;
+
+    // Hard coded map texture width and height in pixel
+    private int map_height = 10800;
+    private int map_width = 21600;
+
+    bool canvas_set = false;
+
+
 
 
     // Use this for initialization
@@ -20,6 +30,22 @@ public class TestCameraScript : MonoBehaviour {
 	void Update () {
 
 
+        // Let the (created) InfoScreens look at the user
+
+        Canvas[] infoscreens = FindObjectsOfType<Canvas>();
+
+        foreach (var infoscreen in infoscreens)
+        {
+            Camera viewer = Camera.main;
+            infoscreen.transform.LookAt(viewer.transform.position);
+            //infoscreen.transform.rotation = Quaternion.LookRotation(viewer.transform.position - infoscreen.transform.position);
+
+        }
+
+        // Predefine lat & lng
+        float lat;
+        float lng;
+
         RaycastHit pointerCollidedWith;
         Ray ray = new Ray(camera.transform.position, camera.transform.forward);
 
@@ -33,25 +59,37 @@ public class TestCameraScript : MonoBehaviour {
                       pointerCollidedWith.collider.name == "Globe")
                 {
 
+                    float x = pointerCollidedWith.textureCoord.x * map_width;
+                    float y = pointerCollidedWith.textureCoord.y * map_height;
 
-                    Debug.DrawRay(camera.transform.position, camera.transform.forward, Color.red);
-                    Debug.Log("Globe hit");
-
-                    Debug.DrawRay(pointerCollidedWith.point, camera.transform.up, Color.green);
-
-
-                    sphereRadius = pointerCollidedWith.transform.GetComponent<SphereCollider>().transform.lossyScale.x;
-
-                    Debug.Log(pointerCollidedWith.point);
+                    lat = y / (map_height / 180) - 90;
+                    lng = x / (map_width / 360) - 180;
 
                     // Call API function from here
-                    getDataFor(pointerCollidedWith.point);
+                    //getDataFor(pointerCollidedWith.point);
 
                 }
 
                 // Do something with the object that was hit by the raycast.
             }
         }
+
+
+
+
+        // Test creation of the info window
+        if (Input.GetKey(KeyCode.I) && !canvas_set) {
+
+
+            CreateCanvas();
+            canvas_set = true;
+        }
+
+        if (Input.GetKey(KeyCode.P)) {
+
+            canvas_set = false;
+        }
+
 
         float scaleInput = 0.1f; // 0.01f
         if (Input.GetKey(KeyCode.RightArrow))
@@ -94,7 +132,7 @@ public class TestCameraScript : MonoBehaviour {
     {
         // First we convert our hitPoint to Latitude and Longitude
 
-        Vector2 loc = vector2LatLng(hitPoint);
+        //Vector2 loc = vector2LatLng(hitPoint);
         //Debug.Log("loc-> " + loc);
         // Then we contact the API to receive the data
         string url = "http://httpbin.org/ip";
@@ -119,30 +157,109 @@ public class TestCameraScript : MonoBehaviour {
         }
     }
 
-    private Vector2 vector2LatLng(Vector3 point)
+    public void CreateCanvas_old()
     {
-        float radius = Mathf.Sqrt(Mathf.Pow(point.x, 2) + Mathf.Pow(point.y, 2) + Mathf.Pow(point.z, 2));
 
-        Debug.Log(point);
+        var canvasCount = FindObjectsOfType<Canvas>().Length;
+        var newCanvasGO = new GameObject("TempCanvas");
+        newCanvasGO.layer = 5;
+        var canvas = newCanvasGO.AddComponent<Canvas>();
+        var canvasRT = canvas.GetComponent<RectTransform>();
+        canvasRT.position = new Vector3(-4f, 2f, 2f + canvasCount);
+        canvasRT.sizeDelta = new Vector2(300f, 400f);
+        canvasRT.localScale = new Vector3(0.005f, 0.005f, 0.005f);
+        //canvasRT.eulerAngles = new Vector3(0f, 0f, 0f);
 
-        Debug.Log(radius);
-        //        float lat = Mathf.Acos(point.y / radius);
-        //        float lng = Mathf.Atan(point.z / point.x);
+        var newButtonGO = new GameObject("TempButton");
+        newButtonGO.transform.parent = newCanvasGO.transform;
+        newButtonGO.layer = 5;
 
-        //calc longitude
-        float lng = Mathf.Atan2(point.x, point.z);
+        newButtonGO.AddComponent<RectTransform>();
 
-        //atan2 does the magic
-        float lat = Mathf.Atan2(-point.y, radius);
+        var buttonRT = newButtonGO.GetComponent<RectTransform>();
+        buttonRT.position = new Vector3(0f, 0f, 0f);
+        buttonRT.anchoredPosition = new Vector3(0f, 0f, 0f);
+        buttonRT.localPosition = new Vector3(0f, 0f, 0f);
+        buttonRT.sizeDelta = new Vector2(180f, 60f);
+        buttonRT.localScale = new Vector3(1f, 1f, 1f);
+        buttonRT.localEulerAngles = new Vector3(0f, 0f, 0f);
 
-        //convert to deg
-        lat *= Mathf.Rad2Deg;
-        lng *= Mathf.Rad2Deg;
+        newButtonGO.AddComponent<Image>();
+        var canvasButton = newButtonGO.AddComponent<Button>();
+        var buttonColourBlock = canvasButton.colors;
+        buttonColourBlock.highlightedColor = Color.red;
+        canvasButton.colors = buttonColourBlock;
 
-        lat *= -1;
-        lng *= -1;
+        var newTextGO = new GameObject("BtnText");
+        newTextGO.transform.parent = newButtonGO.transform;
+        newTextGO.layer = 5;
 
-        Debug.Log("lat->" + lat + " lng->" + lng);
-        return new Vector2(lat, lng);
+        var textRT = newTextGO.AddComponent<RectTransform>();
+        textRT.position = new Vector3(0f, 0f, 0f);
+        textRT.anchoredPosition = new Vector3(0f, 0f, 0f);
+        textRT.localPosition = new Vector3(0f, 0f, 0f);
+        textRT.sizeDelta = new Vector2(180f, 60f);
+        textRT.localScale = new Vector3(1f, 1f, 1f);
+        textRT.localEulerAngles = new Vector3(0f, 180f, 0f);
+
+        var txt = newTextGO.AddComponent<Text>();
+        txt.text = "New Button";
+        txt.color = Color.black;
+        txt.fontSize = 75;
+        txt.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+
+        FindObjectOfType<VRTK_UIPointer>().SetWorldCanvas(canvas);
+    }
+    public void CreateCanvas()
+    {
+        // Limited to one canvas while developing.. DELETE THIS LATER!!
+        if (canvas_set) return;
+
+        // Find out how many info screens have been placed
+        var canvasCount = FindObjectsOfType<Canvas>().Length;
+
+        var newCanvasGO = new GameObject("InfoScreen Prototype");
+
+        var canvas = newCanvasGO.AddComponent<Canvas>();
+        var canvasRT = canvas.GetComponent<RectTransform>();
+
+        canvasRT.localScale = new Vector3(0.005f, 0.005f, 0.005f);
+        //canvasRT.eulerAngles = new Vector3(0f, 0f, 0f);
+
+        canvasRT.position = new Vector3(4.5f, 0.5f, 15f);
+        canvasRT.sizeDelta = new Vector2(500f, 281f);
+
+        var background = newCanvasGO.AddComponent<Image>();
+
+        // Set the background to white + transparent
+        background.color = new Color(1f, 1f, 1f, 0.15f);
+
+        var newTextGO = new GameObject("Text");
+        newTextGO.transform.parent = newCanvasGO.transform;
+
+        var textRT = newTextGO.AddComponent<RectTransform>();
+        textRT.position = new Vector3(0f, 0f, 0f);
+        textRT.anchoredPosition = new Vector3(0f, 0f, 0f);
+        textRT.localPosition = new Vector3(0f, 0f, 0f);
+        textRT.sizeDelta = new Vector2(500f, 281f);
+        textRT.localScale = new Vector3(1f, 1f, 1f);
+        textRT.localEulerAngles = new Vector3(0f, 180f, 0f);
+
+
+
+
+
+
+
+        var txt = newTextGO.AddComponent<Text>();
+
+        txt.text = "InfoScreen";
+        txt.color = Color.black;
+
+        txt.fontSize = 40;
+        txt.font = Resources.GetBuiltinResource(typeof(Font), "Arial.ttf") as Font;
+
+        Debug.Log(FindObjectOfType<Canvas>());
+
     }
 }
