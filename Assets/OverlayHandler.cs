28a61e38d5407d222c2d6a41511a700058d8e05a
Sitcom Lab <sitcomlab@trash-mail.com>
2017-02-02 15:02:58 +0100

    update overlay

--- Assets/OverlayHandler.cs
+++ Assets/OverlayHandler.cs
@@ -18,25 +18,31 @@ public class OverlayHandler : MonoBehaviour {
     // @param type type of overlay, "none" for no overlay (removing the actual one)
     public void addOverlay(string type)
     {
-        string url = "http://localhost:3000/overlay/" + type;
+        //string url = "http://localhost:3000/overlay/" + type;
+        string url = "file:///C:/Users/sitcomlab/Desktop/GlobeVR/Assets/temp_reproj.png";
+        
         WWW www = new WWW(url);
+        Debug.Log(www);
         StartCoroutine(loadImage(www));
        
     }
 
     IEnumerator loadImage(WWW www)
     {
-
+        
         Texture2D tex;
         tex = new Texture2D(128, 256, TextureFormat.DXT1, false);
         yield return www;
         www.LoadImageIntoTexture(tex);
         //GetComponent<Renderer>().material.mainTexture = tex;
+        Debug.Log(tex);
+
 
         GameObject globe = GameObject.FindGameObjectWithTag("Globe");
         Renderer globeRenderer = globe.GetComponent<Renderer>();
         Material globeMaterial = globeRenderer.material;
         globeMaterial.EnableKeyword("_EMISSION");
-        globeMaterial.SetTexture("_EMISSION", tex);
+        // http://answers.unity3d.com/questions/914923/standard-shader-emission-control-via-script.html
+        globeMaterial.SetTexture("_DetailAlbedoMap", tex);
     }
 }
