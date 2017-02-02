8a141e2c32b225b4852a387b5fc642deaade8003
felixerdy <felix.erdmann@uni-muenster.de>
2017-02-02 12:47:50 +0100

    overlay logic, add icons

--- Assets/OverlayHandler.cs
+++ Assets/OverlayHandler.cs
@@ -18,10 +18,25 @@ public class OverlayHandler : MonoBehaviour {
     // @param type type of overlay, "none" for no overlay (removing the actual one)
     public void addOverlay(string type)
     {
+        string url = "http://localhost:3000/overlay/" + type;
+        WWW www = new WWW(url);
+        StartCoroutine(loadImage(www));
+       
+    }
+
+    IEnumerator loadImage(WWW www)
+    {
+
+        Texture2D tex;
+        tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+        yield return www;
+        www.LoadImageIntoTexture(tex);
+        //GetComponent<Renderer>().material.mainTexture = tex;
+
         GameObject globe = GameObject.FindGameObjectWithTag("Globe");
         Renderer globeRenderer = globe.GetComponent<Renderer>();
         Material globeMaterial = globeRenderer.material;
         globeMaterial.EnableKeyword("_EMISSION");
-        //globeMaterial.SetTexture("_EMISSION", GetComponent)
+        globeMaterial.SetTexture("_EMISSION", tex);
     }
 }
